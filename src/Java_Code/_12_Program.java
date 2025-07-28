0610ca935a4cb0eaae4d0af0fdbc2a33ec0e53c4
Sushantlog <sushantbpatil53@gmail.com>
2025-07-28 09:28:48 +0530

    28/07/25 First Commit of Java Programming Code

---
+++ src/Java_Code/_12_Program.java
@@ -0,0 +1,46 @@
+package Java_Code;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+//Palindrome number
+class Palindrome
+{
+    boolean isPalindrome(String str){
+        int left = 0;
+        int right  = str.length() - 1;
+        str = str.toLowerCase();
+
+        while (left < right){
+            if(!Character.isLetterOrDigit(str.charAt(left))){
+                System.out.println(Character.isLetterOrDigit(str.charAt(left)));
+                left++;
+            } else if (!Character.isLetterOrDigit(str.charAt(right))) {
+                right--;
+            }else {
+                if(str.charAt(left) != str.charAt(right)){
+                    return false;
+                }
+                left++;
+                right--;
+            }
+        }
+        return true;
+    }
+}
+
+public class _12_Program {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter The String : ");
+        String input = scanner.nextLine();
+
+        Palindrome palindrome = new Palindrome();
+        if(palindrome.isPalindrome(input)){
+            System.out.println("It is a Palindrome");
+        }else {
+            System.out.println("It is not Palindrome");
+        }
+        scanner.close();
+    }
+}
