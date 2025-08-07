fdbdf30f1482e4f5a5c6fd139e26e99f394961d6
Sushantlog <sushantbpatil53@gmail.com>
2025-08-07 14:36:25 +0530

    07/08/25 First Commit of Java Programming Code

--- src/Java_Code/_19_Program.java
+++ src/Java_Code/_19_Program.java
@@ -7,6 +7,7 @@ import java.util.Scanner;
 class Demo
 {
     int isPrime(int num){
+        if (num <= 1) return 0;
         for(int i=2; i<=Math.sqrt(num); i++){
             if(num%i == 0){
                 return 0;
@@ -19,10 +20,11 @@ class Demo
 public class _19_Program {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter The Number You Want To Find Prime Factors : ");
+        System.out.print("Enter The Number You Want To Find Prime Factors of: ");
         int num = scanner.nextInt();
 
         Demo demo = new Demo();
+        System.out.print("Prime Factors of " + num + " are: ");
         for(int i=2; i<=num; i++){
             if(demo.isPrime(i)==1){
                 int x = num;
