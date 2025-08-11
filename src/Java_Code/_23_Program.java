621142e894bde82aba55066f8531386802f457d9
Sushantlog <sushantbpatil53@gmail.com>
2025-08-11 13:19:43 +0530

    11/08/25 First Commit of Java Programming Code

---
+++ src/Java_Code/_23_Program.java
@@ -0,0 +1,41 @@
+package Java_Code;
+
+//Automorphic number
+//An Automorphic Number (also called a Curious Number or Circular Number) is a
+// number whose square ends with the same digits as the number itself.
+
+import java.util.Scanner;
+
+class FindAutomorphic
+{
+    int isAutomorphic(int num){
+        int square = num * num;
+        while(num != 0){
+            if(num%10 != square%10){
+                return 0;
+            }
+            num = num / 10;
+            square = square / 10;
+        }
+        return 1;
+    }
+}
+
+public class _23_Program {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter The Number : ");
+        int num = scanner.nextInt();
+
+        FindAutomorphic findAutomorphic = new FindAutomorphic();
+
+        int square = num * num;
+
+        if(findAutomorphic.isAutomorphic(num) == 1){
+            System.out.println("Num: "+num+", Square: "+square+" - is Automorphic");
+        }else{
+            System.out.println("Num: "+num+", Square: "+square+" - is not Automorphic");
+        }
+
+    }
+}
